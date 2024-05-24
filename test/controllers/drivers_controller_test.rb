d78b85930e8adb272869c3f5bf5c29cf48ef2d56
roy kimathi <kimathiwaweru@gmail.com>
2024-05-24 16:58:17 +0300

    :sparkles: generate driver scaffold

---
+++ test/controllers/drivers_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class DriversControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @driver = drivers(:one)
+  end
+
+  test "should get index" do
+    get drivers_url, as: :json
+    assert_response :success
+  end
+
+  test "should create driver" do
+    assert_difference("Driver.count") do
+      post drivers_url, params: { driver: { password: @driver.password, username: @driver.username } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show driver" do
+    get driver_url(@driver), as: :json
+    assert_response :success
+  end
+
+  test "should update driver" do
+    patch driver_url(@driver), params: { driver: { password: @driver.password, username: @driver.username } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy driver" do
+    assert_difference("Driver.count", -1) do
+      delete driver_url(@driver), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
