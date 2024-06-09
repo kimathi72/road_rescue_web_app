fb74898fe849075067ede34e370daad632b1af7b
roy kimathi <kimathiwaweru@gmail.com>
2024-06-09 16:17:02 +0300

    :sparkles:update routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Home from "./components/Home.js"
 import Signin from "./components/Signin.js"
 import Signup from "./components/Signup.js"
 import NavBar from "./components/NavBar.js"
+import ViewDriver from "./components/ViewDriver.js"
+import UpdateDriver from "./components/UpdateDriver.js"
+import DeleteDriver from "./components/DeleteDriver.js"
 function App() {
   const [role, setRole] = useState("")
   const [user, setUser] = useState({})
@@ -12,13 +15,12 @@ function App() {
     <Router>
     <NavBar/>
     <Routes>
-
     <Route path='/' element={<Home setRole ={setRole} setUser= {setUser}/>}/> 
     <Route path='/driver/signin' element={<Signin role ={role}/>}/> 
     <Route path="/driver/:id" element={<ViewDriver user = {user} />}/>  
     <Route path="/driver/create" element={<Signup role = {role} />}/> 
     <Route path="/driver/:id/update" element={<UpdateDriver user = {user} />}/>  
-
+    <Route path="/driver/:id/destroy" element={<DeleteDriver user = {user}/>}/>
     </Routes>
   </Router>
   );
