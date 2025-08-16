d8a79b3e7ace0534696907585b9cde03d792de10
roy kimathi <kimathiwaweru@gmail.com>
2025-08-17 01:56:13 +0300

    :sparkles: add small font to message. user .name  icon

--- client/src/components/chat/MessageShow.jsx
+++ client/src/components/chat/MessageShow.jsx
@@ -7,7 +7,7 @@ export default function MessageShow({message, user}) {
   return (
     !!message && <Card className={!!user && (user.id === message.user.id) ? "sent" : "received"}>
       <CardContent >
-        <Typography sx={{color: "text.secondary",textAlign: "left"}}><ContactPageIcon/> {message.user.name || message.user.email}</Typography>
+        <Typography variant='body1' sx={{color: "text.secondary",textAlign: "left"}}><ContactPageIcon fontSize='small' sx={{verticalAlign:"middle", mr: 1}}/> {message.user.name || message.user.email}</Typography>
         <Typography gutterBottom >{message.content}</Typography>
         <Typography variant='caption'><DoneAllIcon/> {new Date(!!message && message['created_at']).toLocaleString()}</Typography>
       </CardContent>
