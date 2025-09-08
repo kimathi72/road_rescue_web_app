9053cdb30b26eecbcaaeddd3b5199288278a614a
roy kimathi <kimathiwaweru@gmail.com>
2025-09-08 11:00:00 +0300

    :bug: remove user loader

--- bin/forth-year-build.sh
+++ bin/forth-year-build.sh
@@ -8,5 +8,7 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
+bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rails db:seed
