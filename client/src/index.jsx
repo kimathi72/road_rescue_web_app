75ae28d49310c38bb967fa957fb56efc5f74c4f0
roy kimathi <kimathiwaweru@gmail.com>
2025-11-25 11:30:30 +0300

    add admin role

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,27 +7,54 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
 import App, { loader as apploader } from "./App";
-import Chat, {loader as chatLoader, action as chatAction} from './components/chat/Chat.jsx'
+import Chat, {
+  loader as chatLoader,
+  action as chatAction,
+} from "./components/chat/Chat.jsx";
 import ErrorPage from "./ErrorPage";
 import Signin, { action as signinAction } from "./components/auth/Signin.jsx";
-import Signup, {action as signupAction} from "./components/auth/Signup.jsx";
+import Signup, { action as signupAction } from "./components/auth/Signup.jsx";
 import Signout, {
   loader as signoutloader,
 } from "./components/auth/Signout.jsx";
- import RequestCreate, {loader as servicesLoader, action as reqAction} from "./components/request/RequestCreate.jsx";
- import RequestQueue, {loader as reqQueLoader} from "./components/request/RequestQueue.jsx";
- import RequestShow, {loader as reqShowLoader, action as reqShowAction} from "./components/request/RequestShow.jsx";
+import RequestCreate, {
+  loader as servicesLoader,
+  action as reqAction,
+} from "./components/request/RequestCreate.jsx";
+import RequestQueue, {
+  loader as reqQueLoader,
+} from "./components/request/RequestQueue.jsx";
+import RequestShow, {
+  loader as reqShowLoader,
+  action as reqShowAction,
+} from "./components/request/RequestShow.jsx";
 import reportWebVitals from "./reportWebVitals";
 import LoadingPage from "./LoadingPage";
-import RequestIndex, {loader as reqIndexLoader} from "./components/request/RequestIndex.jsx";
-import InvoicesList, {loader as invListLoader} from "./components/invoice/InvoiceList.jsx";
-import InvoiceShow , {loader as invShowLoader } from "./components/invoice/InvoiceShow.jsx";
-import InvoiceEdit, {action as reqEditAction} from "./components/invoice/InvoiceEdit.jsx"
-import Reports, {loader as repLoader} from "./components/reports/Reports.jsx";
-import RequestsGroups , {loader as reqGrpLoader, action  as reqGrpAction} from "./components/request/RequestsGroups.jsx";
-import VehicleIndex, {loader as vehLoader, action as vehAction} from "./components/vehicle/VehicleIndex.jsx";
-import RoleIndex, {loader as roleLoader} from "./components/auth/RoleIndex.jsx";
-import Users, {loader as userLoader} from "./components/user/Users.jsx";
+import RequestIndex, {
+  loader as reqIndexLoader,
+} from "./components/request/RequestIndex.jsx";
+import InvoicesList, {
+  loader as invListLoader,
+} from "./components/invoice/InvoiceList.jsx";
+import InvoiceShow, {
+  loader as invShowLoader,
+} from "./components/invoice/InvoiceShow.jsx";
+import InvoiceEdit, {
+  action as reqEditAction,
+} from "./components/invoice/InvoiceEdit.jsx";
+import Reports, { loader as repLoader } from "./components/reports/Reports.jsx";
+import RequestsGroups, {
+  loader as reqGrpLoader,
+  action as reqGrpAction,
+} from "./components/request/RequestsGroups.jsx";
+import VehicleIndex, {
+  loader as vehLoader,
+  action as vehAction,
+} from "./components/vehicle/VehicleIndex.jsx";
+import RoleIndex, {
+  loader as roleLoader,
+} from "./components/auth/RoleIndex.jsx";
+import Users, { loader as userLoader } from "./components/user/Users.jsx";
 
 const router = createBrowserRouter([
   {
@@ -44,9 +71,8 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-          element: <RoleIndex/>, 
-          loader: roleLoader
-           
+        element: <RoleIndex />,
+        loader: roleLoader,
       },
       {
         path: "/signin",
@@ -60,86 +86,89 @@ const router = createBrowserRouter([
       },
       {
         path: "/signup",
-        element: <Signup/>,
+        element: <Signup />,
         action: signupAction,
-      },{
-        path: "/vehicles/",
-        element: <VehicleIndex/>,
-        loader: vehLoader,
-        action: vehAction
       },
-       {
-            path: "/chats/:chatId",
-            element: <Chat/>,
-            loader: chatLoader,
-            action: chatAction
-          }
-      ,{
+      {
+        path: "/vehicles/",
+        element: <VehicleIndex />,
+        loader: vehLoader,
+        action: vehAction,
+      },
+      {
+        path: "/chats/:chatId",
+        element: <Chat />,
+        loader: chatLoader,
+        action: chatAction,
+      },
+      {
         path: "/invoices",
-        children:[
+        children: [
           {
-            index: true, 
-            element: <InvoicesList/>,
-            loader: invListLoader, 
+            index: true,
+            element: <InvoicesList />,
+            loader: invListLoader,
           },
           {
             path: "/invoices/:invoiceId",
-            element: <InvoiceShow />, 
-            loader: invShowLoader, 
-          }, 
+            element: <InvoiceShow />,
+            loader: invShowLoader,
+          },
           {
             path: "/invoices/:invoiceId/edit",
-            element: <InvoiceEdit/>,
-            action: reqEditAction
-          }
-        ]
+            element: <InvoiceEdit />,
+            action: reqEditAction,
+          },
+        ],
       },
       {
-        path:'/reports',
-        element: <Reports/>,
-        loader: repLoader
-      },{
-        path: '/users',
-        element: <Users/>,
-        loader: userLoader
+        path: "/reports",
+        element: <Reports />,
+        loader: repLoader,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+        loader: userLoader,
       },
       {
         path: "/requests/",
         children: [
           {
-            index: true, 
-            element:  <RequestIndex/>,
-        loader: reqIndexLoader,
-        HydrateFallback: ()=> <LoadingPage>
-          <p>fetching requests. . .</p>
-        </LoadingPage>
+            index: true,
+            element: <RequestIndex />,
+            loader: reqIndexLoader,
+            HydrateFallback: () => (
+              <LoadingPage>
+                <p>fetching requests. . .</p>
+              </LoadingPage>
+            ),
           },
           {
             path: "/requests/create",
-            element:<RequestCreate/>,
+            element: <RequestCreate />,
             loader: servicesLoader,
-            action: reqAction
+            action: reqAction,
           },
           {
-            path: "/requests/queue", 
-            element: <RequestQueue/>,
+            path: "/requests/queue",
+            element: <RequestQueue />,
             loader: reqQueLoader,
           },
           {
             path: "/requests/:id",
-            element: <RequestShow/>, 
+            element: <RequestShow />,
             loader: reqShowLoader,
             action: reqShowAction,
-           
-          },{
+          },
+          {
             path: "/requests/grouped/:userId",
-            element: <RequestsGroups/>,
+            element: <RequestsGroups />,
             loader: reqGrpLoader,
-            action: reqGrpAction
-          }
-          
-        ]
-      }
+            action: reqGrpAction,
+          },
+        ],
+      },
     ],
   },
 ]);
